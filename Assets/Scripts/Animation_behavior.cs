a5e393874dfc5c8103f8ffe11d9e1b784da20660
LRubaoL <r_cesar3d@outlook.com>
2018-12-04 15:30:55 -0500

    Before u do something

---
+++ Assets/Scripts/Animation_behavior.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class Animation_behavior : MonoBehaviour {
+    [SerializeField] Animator comboAnim;
+    [SerializeField] Animator bigHitAnim;
+
+    [SerializeField] TextMeshProUGUI hitCountText;
+    [SerializeField] TextMeshProUGUI dmgPercentText;
+
+    public int hitCount;
+    public int dmgPercent;
+
+    void Combo() {
+
+
+    }
+
+
+}

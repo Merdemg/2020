1c6538951fda82ee525461d305e5dee9690efd46
Rubito Lisa <rubens.cesar.junior@hotmail.com>
2018-12-04 11:09:47 -0500

    Traill - Buttons - TextBox

---
+++ Assets/Trail_behavior.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Trail_behavior : MonoBehaviour {
+	[SerializeField] Slider hpBar;
+	[SerializeField] Slider trailBar;
+	[SerializeField] int trailSpeed;
+
+
+	private void Start(){
+		trailBar.maxValue = hpBar.maxValue;
+		trailBar.value = hpBar.value;
+	}
+
+	private void Update(){
+		if (trailBar.value > hpBar.value) {		
+			trailBar.value -= Time.deltaTime * trailSpeed;
+
+		} else if (trailBar.value < hpBar.value) {
+			trailBar.value = hpBar.value;
+		}
+	}
+}
